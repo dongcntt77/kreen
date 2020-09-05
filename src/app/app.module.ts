5ef63fe7424238187bc5addadeae22ad1b05f34c
dongcntt77 <36391847+dongcntt77@users.noreply.github.com>
2020-09-05 13:43:56 +0700

    upade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { MainComponent } from './main/main.component';
+import { DanhmucComponent } from './danhmuc/danhmuc.component';
+import { ChitietComponent } from './chitiet/chitiet.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {
+    path: 'chitiet',
+    component: ChitietComponent,
+  },
+  {
+    path: 'danhmuc',
+    component: DanhmucComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
